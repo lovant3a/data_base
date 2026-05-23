dd7113283fe5159e85305ba5523ef73db3842563
Безруких Анастасия Дмитриевна <nastyabezrukih28@gmail.com>
2026-05-23 18:09:15 +0300

    Update main.cpp

--- data_base/main.cpp
+++ data_base/main.cpp
@@ -6,159 +6,113 @@
 
 using namespace std;
 
-// очистка буфера ввода
-void clear_input() {
+void clearCin() {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
-// показать меню
-void show_menu() {
-    cout << "1. показать все записи" << endl;
-    cout << "2. добавить запись" << endl;
-    cout << "3. удалить запись" << endl;
-    cout << "4. сортировать по имени" << endl;
-    cout << "5. сортировать по id" << endl;
-    cout << "6. сортировать по значению" << endl;
-    cout << "7. сохранить в файл" << endl;
-    cout << "8. загрузить из файла" << endl;
-    cout << "0. выход" << endl;
-    cout << "введите номер команды";
-}
-
-// добавить запись
-void add_record(Database& db) {
-    string name;
-    int id, qty;
-    double value;
-
-    cout << "введите имя: ";
-    cin.ignore();
-    getline(cin, name);
-
-    cout << "введите id: ";
-    while (!(cin >> id)) {
-        cout << "введите целое число: ";
-        clear_input();
-    }
-
-    cout << "введите значение (вещественное): ";
-    while (!(cin >> value)) {
-        cout << "введите число: ";
-        clear_input();
-    }
-
-    cout << "введите количество (целое): ";
-    while (!(cin >> qty)) {
-        cout << "введите целое число: ";
-        clear_input();
-    }
-
-    db.insert(Record(name, id, value, qty));
-}
-
-// удалить запись
-void remove_record(Database& db) {
-    if (db.empty()) {
-        cout << "база пуста" << endl;
-        return;
-    }
-
-    int index;
-    cout << "введите номер записи для удаления (0-" << db.size() - 1 << "): ";
-    cin >> index;
-
-    db.remove(index);
-}
-
-int main(int argc, char* argv[]) {
+int main() {
+    Database db;
+    int choice = 0;
+    string filename;
 
     setlocale(LC_ALL, "Russian.UTF8");
 
-    Database db;
-    string filename = "database.txt";
-
-    // обработка параметров командной строки
-    if (argc > 1) {
-        filename = argv[1];
-        cout << "файл базы данных: " << filename << endl;
-        db.load_from_file(filename);
-    }
-
-    int choice;
+    cout << "=== БАЗА ДАННЫХ ТОВАРОВ ===" << endl;
 
     while (true) {
-        show_menu();
+        cout << "\nменю:\n";
+        cout << "  1. добавить запись\n";
+        cout << "  2. удалить запись по индексу\n";
+        cout << "  3. показать всю таблицу\n";
+        cout << "  4. сортировать по имени (А-Я)\n";
+        cout << "  5. сортировать по имени (Я-А)\n";
+        cout << "  6. сортировать по ID (возр.)\n";
+        cout << "  7. сортировать по ID (убыв.)\n";
+        cout << "  8. сортировать по цене (дешёвые сначала)\n";
+        cout << "  9. сортировать по цене (дорогие сначала)\n";
+        cout << " 10. сохранить в файл\n";
+        cout << " 11. загрузить из файла\n";
+        cout << "  0. выход\n";
+        cout << "выберите команду: ";
 
         if (!(cin >> choice)) {
-            clear_input();
-            cout << "ошибка ввода" << endl;
+            clearCin();
+            cout << "ошибка: введите число от 0 до 11.\n";
             continue;
         }
 
-        switch (choice) {
-        case 0:
-            cout << "выход из программы..." << endl;
-            return 0;
-
-        case 1:
-            db.print_table();
-            break;
-
-        case 2:
-            add_record(db);
-            break;
-
-        case 3:
-            remove_record(db);
-            break;
-
-        case 4: {
-            if (db.empty()) { cout << "база пуста" << endl; break; }
-            cout << "направление (1 - возрастание, 2 - убывание): ";
-            int dir; cin >> dir;
-            db.sort_name(dir == 1);
-            break;
-        }
-        case 5: {
-            if (db.empty()) { cout << "база пуста" << endl; break; }
-            cout << "направление (1 - возрастание, 2 - убывание): ";
-            int dir; cin >> dir;
-            db.sort_id(dir == 1);
-            break;
-        }
-        case 6: {
-            if (db.empty()) { cout << "база пуста" << endl; break; }
-            cout << "направление (1 - возрастание, 2 - убывание): ";
-            int dir; cin >> dir;
-            db.sort_value(dir == 1);
-            break;
-        }
-
-        case 7: {
-            string fname;
-            cout << "имя файла (Enter - " << filename << "): ";
-            cin.ignore();
-            getline(cin, fname);
-            if (!fname.empty()) filename = fname;
-            db.save_to_file(filename);
-            break;
-        }
-
-        case 8: {
-            string fname;
-            cout << "имя файла: ";
-            cin.ignore();
-            getline(cin, fname);
-            if (!fname.empty()) {
-                db.load_from_file(fname);
-                filename = fname;
+        try {
+            switch (choice) {
+            case 1: {
+                Record rec;
+                cout << "введите название товара: ";
+                clearCin();
+                getline(cin, rec.name);
+                
+                cout << "введите ID (число): ";
+                if (!(cin >> rec.id)) { clearCin(); throw runtime_error("ошибка ввода ID"); }
+                
+                cout << "введите цену: ";
+                if (!(cin >> rec.value)) { clearCin(); throw runtime_error("ошибка ввода цены"); }
+                
+                cout << "введите количество: ";
+                if (!(cin >> rec.quantity)) { clearCin(); throw runtime_error("ошибка ввода количества"); }
+                
+                db.insert(rec);
+                break;
             }
-            break;
+            case 2: {
+                if (db.empty()) {
+                    cout << "база пуста, удалять нечего" << endl;
+                    break;
+                }
+                cout << "введите индекс записи для удаления (0.." << db.size() - 1 << "): ";
+                size_t idx;
+                if (!(cin >> idx) || idx >= db.size()) {
+                    clearCin();
+                    cout << "неверный индекс" << endl;
+                }
+                else {
+                    db.remove(idx);
+                }
+                break;
+            }
+            case 3: {
+                db.print_table();
+                break;
+            }
+            case 4: db.sort_name(true); break;
+            case 5: db.sort_name(false); break;
+            case 6: db.sort_id(true); break;
+            case 7: db.sort_id(false); break;
+            case 8: db.sort_value(true); break;
+            case 9: db.sort_value(false); break;
+            case 10: {
+                cout << "введите имя файла для сохранения: ";
+                clearCin();
+                getline(cin, filename);
+                db.save_to_file(filename);
+                break;
+            }
+            case 11: {
+                cout << "введите имя файла для загрузки: ";
+                clearCin();
+                getline(cin, filename);
+                db.load_from_file(filename);
+                break;
+            }
+            case 0: {
+                cout << "завершение работы. до свидания!" << endl;
+                return 0;
+            }
+            default:
+                cout << "неизвестная команда" << endl;
+            }
         }
-
-        default:
-            cout << "неверный выбор" << endl;
+        catch (const exception& e) {
+            cerr << "\nОШИБКА: " << e.what() << "\n";
+            clearCin();
         }
     }
 
